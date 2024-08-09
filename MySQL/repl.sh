dc0b35f2976a1c2647ff1ab9afec638e04c7dabc
NikPuskov <nikpus@mail.ru>
2024-08-09 17:00:26 +0300

    Update repl.sh

--- MySQL/repl.sh
+++ MySQL/repl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MYSQL='mysql -uroot -psuperuser -h127.0.0.1 -P3306'
+MYSQL='mysql -uroot -psuperuser -h127.0.0.1 -P3305'
 SLAVE='mysql -uroot -psuperuser -h127.0.0.1 -P3308'
 $MYSQL -e "CREATE USER repl@'%' IDENTIFIED WITH 'caching_sha2_password' BY 'superuser';";
 $MYSQL -e "GRANT REPLICATION SLAVE ON *.* TO repl@'%';";
