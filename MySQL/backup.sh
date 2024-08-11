e8a4cdaa5e84ef05897f768855c7254e1f419d0e
NikPuskov <nikpus@mail.ru>
2024-08-11 07:27:43 +0300

    Update backup.sh

--- MySQL/backup.sh
+++ MySQL/backup.sh
@@ -4,12 +4,12 @@ MYSQL='mysql -uroot -psuperuser -h127.0.0.1 -P3308 --skip-column-names';
 
 $MYSQL -e "STOP SLAVE;";
 
-for s in `$MYSQL -e "SHOW DATABASES LIKE '%\_db'"`;
+for s in `$MYSQL -e "SHOW DATABASES`;
     do
         mkdir $s;
         for t in `$MYSQL -e "SHOW TABLES FROM $s"`;
                 do
-                        /usr/bin/mysqldump -uroot -psuperuser -h127.0.0.1 -P3308 --add-drop-table --add-locks --create-options --disable-keys --extended-insert --single-transaction --quick --set-charset --events --routines --triggers $s $t | gzip -1 > $s/$t.gz;
+                        /usr/bin/mysqldump -uroot -psuperuser -h127.0.0.1 -P3308 --all-databases --events --routines --source-data=2 $s $t | gzip -1 > $s/$t.gz;
                 done
     done
 $MYSQL -e "START SLAVE;";
