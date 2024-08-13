e4570f1011fe5ca438e912594cd58821551db432
NikPuskov <nikpus@mail.ru>
2024-08-13 11:37:58 +0300

    Update backup.sh

--- MySQL/backup.sh
+++ MySQL/backup.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
-MYSQL='mysql -uroot -psuperuser -h127.0.0.1 -P3308 --skip-column-names';
+MYSQL='mysql -h127.0.0.1 -P3308 --skip-column-names';
 
 $MYSQL -e "STOP SLAVE;";
 
-for s in `$MYSQL -e "SHOW DATABASES`;
+for s in `$MYSQL -e "SHOW DATABASES"`;
     do
         mkdir $s;
         for t in `$MYSQL -e "SHOW TABLES FROM $s"`;
                 do
-                        /usr/bin/mysqldump -uroot -psuperuser -h127.0.0.1 -P3308 --all-databases --events --routines --source-data=2 $s $t | gzip -1 > $s/$t.gz;
+                        /usr/bin/mysqldump -h127.0.0.1 -P3308 --all-databases --events --routines --source-data=2 $s $t | gzip -1 > $s/$t.gz;
                 done
     done
 $MYSQL -e "START SLAVE;";
