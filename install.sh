b9cbde3c0e0b563761201533a8588443d7939996
NikPuskov <nikpus@mail.ru>
2024-08-18 08:33:16 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+apt install docker.io
+systemctl enable --now docker
+docker pull nginx:1.26
+docker pull httpd:2.4.62
+docker pull mysql:8.0
+docker pull prom/prometheus
+docker pull prom/node-exporter
+docker pull grafana/grafana
+docker pull elasticsearch:8.15.0
+docker pull logstash:8.15.0
+docker pull kibana:8.15.0
+docker pull elastic/filebeat:8.15.0
+apt install git
+apt install mysql-client-core-8.0
