54e2c7fed1b3927a374542edf516c73f1ee825c5
NikPuskov <nikpus@mail.ru>
2024-10-21 14:06:32 +0300

    Delete mysql.sh

--- mysql.sh
+++
@@ -1,6 +0,0 @@
-bash /root/proj/MySQL/repl.sh
-echo "\033[32m Скрипт репликации MySQL выполнен успешно"
-tput sgr0
-bash /root/proj/MySQL/backup.sh
-echo "\033[32m Скрипт бэкапа БД с MySQL-Slave выполнен успешно"
-tput sgr0
